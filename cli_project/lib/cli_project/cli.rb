55ac08fe776f4ac0b2c56a5bd7d1235d12fc9f3a
Colleen Halpenny <cp.halpenny@gmail.com>
2019-09-05 06:39:32 +0000

    Automatically backed up by Learn

--- cli_project/lib/cli_project/cli.rb
+++ cli_project/lib/cli_project/cli.rb
@@ -20,6 +20,8 @@ module CliProject
           menu
           elsif @input == "1"
           puts "ingredients or method to iterate across recipes to pull all available recipes to pull from.  using @@all ?"
+         #these can be managed by a method as well, then defined down below
+         #print ingredients
           elsif @input == "2"
           puts "something else"
           elsif @input == "exit" || @input == "quit"
@@ -38,6 +40,10 @@ module CliProject
        or type "exit" or "quit" at any time to exit the program.
        LIST
      end
+     
+     #def print_ingreient
+     
+     
      
       #instructions - enter an ingredient(s), keywords, 
       #return a list of recipes including that ingredient
